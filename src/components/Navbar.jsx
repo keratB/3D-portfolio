be0391002edee4878b520a269942d829d6263529
kerat Issa <keratmy@yahoo.com>
2023-11-20 15:02:48 -0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+// import { logo } from "../assets/images";
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <NavLink to='/' className="w-10 h-10 rounded-lg bg-white
+      items-center justify-center flex font-bold shadow-md">
+      <p className="blue-gradient_text">KI</p>
+        {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
